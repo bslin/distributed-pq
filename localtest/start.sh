c9eeefa1ebedfcd3dad69bbb8530e8250d80759a
Bill Lin <bilin@bilin-mn2.linkedin.biz>
2020-05-23 22:49:52 -0700

    fix bash script for starting servers

--- localtest/start.sh
+++ localtest/start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 PORT=8080
 NUM=10
-for i in {0..9}
+NUM_1=$((NUM -1))
+for i in $(seq 0 $NUM_1)
 do
   echo "START $i"
   java -Dserver.port=$((PORT + i)) -jar target/priorityqueue-0.0.1.war $i $NUM &
