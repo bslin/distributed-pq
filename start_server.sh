22982684d3971d55f7bafe4aff08fc37ad272316
Xiaohua Liang <lxhfirenking@hotmail.com>
2020-05-25 00:04:12 -0700

    vm setup and server launching scripts for aws deployment

---
+++ start_server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo instance_num: $1
+echo total_instance_num $2
+rm -rf /tmp/kafka-logs
+cd ~/cs244b/kafka_2.12-2.5.0
+sed -i "s/broker.id=0/broker.id=$1/g" config/server.properties
+sed -i 's/zookeeper.connect=localhost:2181/zookeeper.connect=172.31.15.144:2181/g' config/server.properties
+bin/kafka-server-start.sh config/server.properties &
+# creating topics 
+bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --topic add-pq-item-$1 --replica-assignment $1
+# launch server instance 
+cd ~/cs244b/final_project/priorityqueue/
+java -jar target/priorityqueue-0.0.1.war $1 $2
---
+++ vm_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y emacs
+sudo apt-get install -y openjdk-11-jdk
+echo JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/ >> ~/.bashrc
+mkdir -p cs244b/final_project
+cd ~/cs244b/final_project/
+git init .
+git remote add origin https://github.com/bslin/distributed-pq.git
+git pull origin master
+cd ~/cs244b/final_project/priorityqueue/
+./mvnw package
+cd ~/cs244b
+curl -o kafka_2.12-2.5.0.tgz http://mirror.metrocast.net/apache/kafka/2.5.0/kafka_2.12-2.5.0.tgz
+tar -xzf kafka_2.12-2.5.0.tgz
+rm -rf kafka_2.12-2.5.0.tgz
