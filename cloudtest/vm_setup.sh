2c47217c0a90fcb7f61d4baeac2929d37d29eccc
Xiaohua Liang <lxhfirenking@hotmail.com>
2020-05-25 14:55:09 -0700

    minot script update

--- cloudtest/vm_setup.sh
+++ cloudtest/vm_setup.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install -y emacs
 sudo apt-get install -y openjdk-11-jdk
 echo JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/ >> ~/.bashrc
-mkdir -p cs244b/final_project
+mkdir -p ~/cs244b/final_project
 cd ~/cs244b/final_project/
 git init .
 git remote add origin https://github.com/bslin/distributed-pq.git
@@ -11,6 +11,6 @@ git pull origin master
 cd ~/cs244b/final_project/priorityqueue/
 ./mvnw package
 cd ~/cs244b
-curl -o kafka_2.12-2.5.0.tgz http://mirror.metrocast.net/apache/kafka/2.5.0/kafka_2.12-2.5.0.tgz
+curl -o kafka_2.12-2.5.0.tgz http://mirrors.ibiblio.org/apache/kafka/2.5.0/kafka_2.12-2.5.0.tgz
 tar -xzf kafka_2.12-2.5.0.tgz
 rm -rf kafka_2.12-2.5.0.tgz
